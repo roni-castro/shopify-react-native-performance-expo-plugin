2b775715f2eaebd9701b68fa07b4ea64e1f06974
Roni Castro <roni.c.castro@gmail.com>
2024-11-13 22:22:40 -0300

    fix: renaming ios folders

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import { ConfigPlugin } from "@expo/config-plugins";
+import { ConfigPlugin, createRunOncePlugin } from "@expo/config-plugins";
 
 import { withAndroidMainApplicationSetup } from "./withPerformance.android";
 import { withIOSAppDelegateSetup } from "./withPerformance.ios";
 
 const withShopifyPerformance: ConfigPlugin<{ name?: string }> = (
   config,
-  { name = "shopify-react-native-performance-expo-plugin" } = {},
+  _props,
 ) => {
   config = withAndroidMainApplicationSetup(config);
   config = withIOSAppDelegateSetup(config);
-  config.name = name;
 
   return config;
 };
 
-export default withShopifyPerformance;
+export default createRunOncePlugin(
+  withShopifyPerformance,
+  "shopify-react-native-performance-expo-plugin",
+);
